fdbe52031343004758e2c66e90ac815c102944e8
lucyabalo208-spec <lucyabalo208@gmail.com>
2026-09-06 21:23:00 +0300

    Add files via upload

---
+++ OPERATOR.py
@@ -0,0 +1,24 @@
+number1 =int(input("Enter the first nmuber"))
+number2 = float(input("Enter the second number"))
+
+#Addition
+sum = number1 + number2
+print(sum)
+
+number1 = 10
+number2 = 6
+
+#Subtraction
+differnce = number1 - number2
+print (differnce)
+
+#Modulus
+modulus = number1#number2
+print(modulus)
+
+#Concentration
+print (f"The first number is {number1}, and the second number is {number2}")
+firstName = " Lucy"
+secondName = "Abalo"
+
+print (f"My first name is {firstName} and my second name is {secondName}")
