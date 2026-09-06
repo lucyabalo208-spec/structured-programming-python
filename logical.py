46f23d339e0982951eddf108063f67485bfcae73
lucyabalo208-spec <lucyabalo208@gmail.com>
2026-09-06 21:18:36 +0300

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,11 @@
+print(True or False)
+print ( True and False)
+
+
+Test1 = eval(input(" Enter score in test I:"))
+Test2 = eval( input("Enter score in test II:"))
+Test3 = eval(input("Enter score in test III:"))
+avg = (Test1 + Test2 + Test3)
+print ('Your Average scoreis:', avg)
+if avg>=90:
+     print('Congratulations on a High Average')
