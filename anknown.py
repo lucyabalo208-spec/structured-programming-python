dc1cbc6bb0a5f6d629a0de5dcf6d068c53636994
lucyabalo208-spec <lucyabalo208@gmail.com>
2026-09-09 11:17:42 +0300

    Add files via upload

---
+++ anknown.py
@@ -0,0 +1,50 @@
+
+from stanfordkarel import *
+print('Karel library imported succesfull!')
+
+def main():
+    turn_left()
+    pick_beeper()
+    move_up()
+    turn_left()
+    move_right()
+    pick_beeper
+    turn_left()
+    move_up()
+    turn_left()
+    turn_left()
+    turn_left()
+    move_right()
+    turn_right()
+    pick_beeper()
+    move_up()
+    turn_left()
+    move_right()
+    pick_beeper()
+    turn_left()
+    move_up()
+
+
+def move_up():
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+
+def move_right():
+    move()
+    move()
+    move()
+    move()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+if _name_ == 'main':
+ run_karel_program('stone_mason_karel_end')
